160f226da45c64e2ab613ce1b811b55a92b86ed1
namakgs <tranducthong6566@gmail.com>
2025-06-01 19:04:09 +0700

    Update

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
-
+var letterContent =" Chúc em vi ngày 1/6 thật đặc biệt, tớ hy vọng cậu sẽ nhận được nhiều niềm vui và những món quà ý nghĩa.
+Ngày Quốc tế Thiếu nhi, tớ muốn gửi đến cậu 1000 niềm vui và những điều tốt đẹp nhất. Hãy luôn vui vẻ và hạnh phúc nhé!
+Cậu là người dễ thương xinh đẹp là ánh sáng trong cuộc sống của tui á 😉. Chúc em vi có một ngày 1/6 tràn đầy tiếng cười và những kỷ niệm đẹp.
+Nhân ngày 1/6, tui chúc em vi luôn mạnh khỏe, xinh đẹp và đạt được mọi điều em mong muốn. Tớ sẽ luôn bên cạnh cậu !
+Hãy để ngày hôm nay là một ngày thật tuyệt vời, và những kỉ niệm đẹp. 
+Tương lai sau này không biết chúng ta sẽ ra sao như thế nào nhưng tớ rất quý mến cậu rất nhìu rất nhìu 🥰
+Tớ thật sự cảm ơn cậu tớ rất vui khi có cậu cùng đồng hành với tớ trong khoảng thời gian qua .
+Tuy có nhiều chuyện buồn vui lẫn lộn nhưng tớ sẽ luôn luôn rất quý cậu ạ .
+Người vừa dễ thương xinh gái lại còn tốt bụng nữa .
+nói chung là tớ thật sự rất yêu quý cậu "
+    
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
 
