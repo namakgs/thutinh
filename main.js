7033227c46456ab0ba7cc614001186dcac6b13a5
namakgs <tranducthong6566@gmail.com>
2025-06-30 02:35:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
-
+var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của tớ❤️. Cảm ơn cậu đã đến bên tớ những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có cậu. cậu làm cho tớ luôn cảm thấy đặc biệt và hoàn hảo. tớ chẳng biết nói gì ngoài lời cảm ơn cậu, tớ rất yêu quý cậu💕"
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
 
